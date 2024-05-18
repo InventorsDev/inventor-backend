3c11b3ef9c39afd2c2e0500e65735e09b0a3f718
Stefano <snebo54@hotmail.com>
2024-05-18 10:02:52 +0100

    add create function

--- src/lead_registration/lead_registration.service.spec.ts
+++ src/lead_registration/lead_registration.service.spec.ts
@@ -1,7 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LeadRegistrationService } from './lead_registration.service';
-import { Injectable, NotFoundException} from '@nestjs/common';
-import { CreateLeadRegistrationDto } from './dto/create-lead_registration.dto';
 
 describe('LeadRegistrationService', () => {
   let service: LeadRegistrationService;
--- src/lead_registration/lead_registration.service.ts
+++ src/lead_registration/lead_registration.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException} from '@nestjs/common';
+import { CreateLeadRegistrationDto } from './dto/create-lead_registration.dto';
 @Injectable()
-export class LeadRegistrationService {}
+export class LeadRegistrationService {
+  private registrations = [] // should have database logic here?
+  
+  // the create function retuns the user registration
+  create(userId: string, CreateLeadRegistrationDto: CreateLeadRegistrationDto){
+    // should have user verification logic here
+    // ie userExists = true;
+    let userExists = function verifyfunc(){return true} // this should be the functions
+
+    if (!userExists){
+      // throw error
+      throw new NotFoundException(`user with Id ${userId} not found`);
+    }
+    const newRegistration = {
+      id: this.registrations.length+1, 
+      userId, 
+      applicationId: CreateLeadRegistrationDto.applicationId,
+      createdAt: new Date(),
+    };
+    this.registrations.push(newRegistration)
+    return newRegistration
+  }
+}
