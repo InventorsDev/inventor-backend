0c4cbccc060544a7a823418b945f4f156b6da07a
Stefano <snebo54@hotmail.com>
2024-05-23 12:41:42 +0100

    move lead schema to shared

--- src/lead_registration/lead_registration.module.ts
+++ src/lead_registration/lead_registration.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LeadRegistrationService } from './lead_registration.service';
 import { LeadRegistrationController } from './lead_registration.controller';
-import { Registration, RegistrationSchema } from './schemas/lead_registration.schema';
+import { Registration, RegistrationSchema } from 'src/shared/schema/lead_registration.schema';
 import { UsersModule } from '../users/users.module'; // Import the UsersModule
 
 
