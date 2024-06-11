c6f756dfccbd94db5a18252e77029ea50bc1e0dc
stefano <30480337+snebo@users.noreply.github.com>
2024-06-12 00:07:39 +0100

    create tempLapLead schema

---
+++ src/shared/schema/tempLead.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { UserRole } from "../interfaces";
+
+@Schema()
+export class TempLeadRegistration extends Document { 
+  @Prop ({required: true})
+  email: string;
+
+  @Prop ({required: true})
+  userId: string;
+
+  @Prop ({required: true})
+  CreatedAt: Date;
+
+  @Prop ({required: true})
+  leadPosition: string;
+}
+export const TempLeadSchema = SchemaFactory.createForClass(TempLeadRegistration);
