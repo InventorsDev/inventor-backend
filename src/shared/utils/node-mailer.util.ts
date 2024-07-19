431d66627cc247621abce743f28bfde85ff8492f
stefano <30480337+snebo@users.noreply.github.com>
2024-07-19 14:26:55 +0100

    add log for failed emails

--- src/shared/utils/node-mailer.util.ts
+++ src/shared/utils/node-mailer.util.ts
@@ -1,23 +1,9 @@
-// export const getTemplateKey = (templateValue) => {
-//   const templates = getMailTemplate();
-//   return Object.entries(templates).filter(
-//     ([key, value]) => value === templateValue,
-//   )?.[0]?.[0];
-// };
-
-// const readFileSync = promisify(readFile);
-
-// // let transporter: nodemailer.Transporter;
-// let templateCache: {
-//   [templateName: string]: handlebars.TemplateDelegate;
-// };
-
-// const nodeMailer = configs().node_mailer;
 import * as nodemailer from 'nodemailer';
 import * as handlebars from 'handlebars';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { configs } from '../configs';
+import { LogLevel } from '../interfaces';
 
 const nodeMailer = configs().node_mailer;
 export const getMailTemplate = () => configs().node_mailer.templates;
@@ -25,7 +11,7 @@ export const getMailTemplate = () => configs().node_mailer.templates;
 const transporter = nodemailer.createTransport({
   host: nodeMailer.host,
   port: nodeMailer.port,
-  secure: nodeMailer.port === 465, // true for 465, false for other ports
+  secure: nodeMailer.port === 465, // mailtrap should be false
   auth: {
     user: nodeMailer.auth.user,
     pass: nodeMailer.auth.pass,
@@ -33,7 +19,6 @@ const transporter = nodemailer.createTransport({
 });
 
 function loadTemplate(templateName: string, variables: any): string {
-  console.log('dir: ', process.cwd());
   const templatePath = join(
     process.cwd(),
     'src',
@@ -63,10 +48,24 @@ export async function sendMail(options: {
   };
 
   try {
-    console.log(transporter);
     const info = await transporter.sendMail(mailOptions);
     console.log('Email sent: ' + info.response);
   } catch (error) {
     console.error('Error sending email: ' + error.message);
+    global.dataLogService.log(
+      'NodeMailerSendMail',
+      {
+        source: 'NodeMailerSendMail',
+        to: options.to,
+        from: options.from,
+        subject: options.subject,
+        template: options.template,
+        templateVariables: options.templateVariables,
+        stack: error.status,
+        message: error.message,
+      },
+      LogLevel.ERROR,
+    );
+    return false;
   }
 }
