fb9766f13ef4492ef094ddea9ef10989ed668b9d
Anthony Adedayo <tonykay001@gmail.com>
2024-10-14 21:50:22 +0100

    Fix-Create-Event-Issue

--- src/events/events.users.service.ts
+++ src/events/events.users.service.ts
@@ -16,7 +16,19 @@ export class EventService {
   ) {}
 
   async createEvent(payload: EventDto): Promise<Event> {
-    return this.eventModel.create({ payload });
+       return this.eventModel.create({
+         title: payload.title,
+         shortDesc: payload.shortDesc,
+         description: payload.description,
+         host: payload.host,
+         coHost: payload.coHost,
+         location: payload.location,
+         photo: payload.photo,
+         joinMethod: payload.joinMethod,
+         link: payload.link,
+         socialsLinks: payload.socialsLinks,
+         eventDate: payload.eventDate,
+       });
   }
 
   async findAll(req: ApiReq) {
