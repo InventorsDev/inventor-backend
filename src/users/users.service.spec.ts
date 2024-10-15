b63524a1e1458732268b71396865f6f232a23593
stefano <30480337+snebo@users.noreply.github.com>
2024-10-15 22:25:09 +0100

    add test case

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,20 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { TestModule } from 'src/shared/testkits';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/shared/schema';
+import { BcryptUtil } from 'src/shared/utils';
+import { RegistrationMethod } from 'src/shared/interfaces';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let userModel: any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [TestModule],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getModelToken(User.name), useValue: userModel },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userModel = {
+      findOne: jest.fn(),
+      create: jest.fn(),
+      findOneAndUpdate: jest.fn(),
+      findByIdAndDelete: jest.fn(),
+      countDocuments: jest.fn(), // what are we counting here
+    };
+
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('userInvite', () => {
+    it('should crate a user invite and return the user object', async () => {
+      const payload = {
+        email: 'test@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        roles: ['ADMIN'],
+        JoinMethod: RegistrationMethod.INVITATION,
+      };
+      const createUser = {
+        _id: 'someId',
+        ...payload,
+        password: 'hashed password',
+      };
+
+      jest
+        .spyOn(BcryptUtil, 'generateHash')
+        .mockResolvedValue('hashed_password');
+      userModel.create.mockResolvedValue(createUser);
+      const ressult = await usersService.userInvite(payload);
+
+      expect(ressult).toEqual(createUser);
+      expect(BcryptUtil.generateHash).toHaveBeenCalled();
+      expect(userModel.create).toHaveBeenCalledWith({
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'test@example.com',
+        role: ['ADMIN'],
+        emailVerification: true,
+        password: 'hashed_password',
+      });
+    });
   });
 });
