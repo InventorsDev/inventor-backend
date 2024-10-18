70f224dc36a6672e0280894c51614a7f5af75001
stefano <30480337+snebo@users.noreply.github.com>
2024-10-18 20:45:56 +0100

    setup test for admin controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,21 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TestModule } from 'src/shared/testkits';
+import { UsersAdminsController } from './users.admin.controller';
+import { DBModule } from 'src/shared/schema';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let adminController: UsersAdminsController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TestModule],
-      controllers: [UsersController],
+      imports: [TestModule, DBModule],
+      controllers: [UsersController, UsersAdminsController],
       providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    adminController = module.get<UsersAdminsController>(UsersAdminsController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(adminController).toBeDefined();
   });
 });
