23a4a10c4c0cf5d7d0a2899c9f366eaf6d931e76
stefano <30480337+snebo@users.noreply.github.com>
2024-11-09 20:25:31 +0100

    fix change password test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -17,6 +17,51 @@ import { IPageable } from 'src/shared/utils';
 type UserDocument = Document<unknown, {}, User> &
   User & { _id: Types.ObjectId };
 
+/*
+'*generates a mock user object that can be used across multiple tests
+ *choose which part you want to override using createMock({parameter:new_value})
+*/
+
+const createUserMock = (
+  overrides: Partial<UserDocument> = {},
+): UserDocument => {
+  return {
+    _id: new Types.ObjectId(),
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    password: 'hashedpassword123',
+    profileSummary: 'Experienced software engineer',
+    jobTitle: 'Senior Developer',
+    currentCompany: 'TechCorp',
+    photo: 'profilephoto.jpg',
+    age: 30,
+    phone: '123-456-7890',
+    userHandle: 'johnDoe123',
+    gender: 'male',
+    location: {
+      type: 'Point',
+      coordinates: [40.7128, -74.006],
+    },
+    deviceId: 'device12345',
+    deviceToken: 'deviceToken12345',
+    role: [UserRole.USER],
+    leadPosition: 'Tech Lead',
+    applicationStatus: ApplicationStatus.PENDING,
+    nextApplicationTime: new Date(),
+    joinMethod: RegistrationMethod.SIGN_UP,
+    status: UserStatus.ACTIVE,
+    emailVerification: true,
+    pendingInvitation: false,
+    socials: {
+      phoneNumber: '24242424',
+      email: 'balbal',
+    },
+    nextVerificationRequestDate: new Date(),
+    ...overrides, // Overrides allow customization of the mock
+  } as UserDocument;
+};
+
 describe('UsersAdminController', () => {
   let controller: UsersController;
   let adminController: UsersAdminsController;
@@ -240,78 +285,36 @@ describe('UsersAdminController', () => {
       expect(usersService.findAll).toHaveBeenCalledWith(requestMock);
     });
   });
-  // describe('change Password', () => {
-  //   const mockUser = createUserMock({_id: new Types.ObjectId(),});
-  //   const mockRequest = {
-  //     user: {
-  //       _id: mockUser._id,
-  //       email: mockUser.email,
-  //       role: [UserRole.ADMIN],
-  //     },
-  //   };
-  //   const UserChangePasswordDto = {
-  //     oldPassword: 'oldPassword@123'
-  //     newPassword: 'newPassword@123',
-  //     confirmPassword: 'newPassword@123',
-  //   };
+  describe('change Password', () => {
+    const mockUser = createUserMock({ _id: new Types.ObjectId() });
+    const mockRequest = {
+      user: {
+        _id: mockUser._id,
+        email: mockUser.email,
+        role: [UserRole.ADMIN],
+      },
+    };
+    const UserChangePasswordDto = {
+      oldPassword: 'oldPassword@123',
+      newPassword: 'newPassword@123',
+      confirmPassword: 'newPassword@123',
+    };
 
-  //   it('should change the user password', async () => {
-  //     const updatedUser = { ...mockUser, password: 'newHashedPassword' };
-  //     jest.spyOn(usersService, 'changePassword').mockResolvedValue(updatedUser);
-  //     const result = await adminController.changePassword(
-  //       mockRequest,
-  //       mockUser._id.toString(),
-  //       UserChangePasswordDto,
-  //     );
-  //     expect(result).toEqual(updatedUser);
-  //     expect(usersService.changePassword).toHaveBeenLastCalledWith(
-  //       mockRequest,
-  //       mockUser._id.toString(),
-  //       UserChangePasswordDto,
-  //       true,
-  //     );
-  //   });
-  // });
+    it('should change the user password', async () => {
+      const updatedUser = { ...mockUser, password: 'newHashedPassword' };
+      jest.spyOn(usersService, 'changePassword').mockResolvedValue(updatedUser);
+      const result = await adminController.changePassword(
+        mockRequest,
+        mockUser._id.toString(),
+        UserChangePasswordDto,
+      );
+      expect(result).toEqual(updatedUser);
+      expect(usersService.changePassword).toHaveBeenLastCalledWith(
+        mockRequest,
+        mockUser._id.toString(),
+        UserChangePasswordDto,
+        true,
+      );
+    });
+  });
 });
-
-/*
-'*generates a mock user object that can be used across multiple tests
- *choose which part you want to override using createMock({parameter:new_value})
-*/
-const createUserMock = (overrides: Partial<User> = {}): User => {
-  return {
-    // _id: new Types.ObjectId(),
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john.doe@example.com',
-    password: 'hashedpassword123',
-    profileSummary: 'Experienced software engineer',
-    jobTitle: 'Senior Developer',
-    currentCompany: 'TechCorp',
-    photo: 'profilephoto.jpg',
-    age: 30,
-    phone: '123-456-7890',
-    userHandle: 'johnDoe123',
-    gender: 'male',
-    location: {
-      type: 'Point',
-      coordinates: [40.7128, -74.006],
-    },
-    deviceId: 'device12345',
-    deviceToken: 'deviceToken12345',
-    role: [UserRole.USER],
-    leadPosition: 'Tech Lead',
-    applicationStatus: ApplicationStatus.PENDING,
-    nextApplicationTime: new Date(),
-    joinMethod: RegistrationMethod.SIGN_UP,
-    status: UserStatus.ACTIVE,
-    emailVerification: true,
-    pendingInvitation: false,
-    socials: {
-      phoneNumber: '24242424',
-      email: 'balbal',
-    },
-    nextVerificationRequestDate: new Date(),
-    ...overrides, // Overrides will allow you to customize the mock as needed
-  };
-};
