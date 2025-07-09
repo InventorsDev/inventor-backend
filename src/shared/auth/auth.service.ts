40e570f5764b4f92ec69c971e9b3422db6ef7682
sirkenedy <owoputikehinde@gmail.com>
2025-07-09 21:12:16 +0100

    Add refresh token feature

--- src/shared/auth/auth.service.ts
+++ src/shared/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
-import { ApiReq, UserStatus } from '../interfaces';
+import { ApiReq, ReqType, UserStatus } from '../interfaces';
 import { User, UserDocument } from '../schema/user.schema';
-import { BcryptUtil, firstCapitalize } from '../utils';
+import { faker } from '@faker-js/faker';
+import { BcryptUtil, firstCapitalize, isMobile, redisGet, redisSet } from '../utils';
+import { RefreshTokenDto } from 'src/users/dto/refresh-token.dto';
 
 @Injectable()
 export class AuthService {
@@ -61,7 +63,29 @@ export class AuthService {
   async login(req: ApiReq) {
     const user: User & any = req.user;
     const payload = this.mapUserToPayload(user);
-    const accessToken = this.generateToken(payload);
+    const accessToken = this.generateToken(req, payload);
+    return { ...accessToken, ...payload };
+  }
+
+  async refreshToken(req: ApiReq, refreshToken: RefreshTokenDto) {
+    const tokenType = isMobile(req) ? ReqType.MOBILE : ReqType.WEB;
+    const oldRefreshKey = `Refresh_token:${tokenType}:${refreshToken}`;
+    const tokenData = await redisGet(oldRefreshKey);
+
+    if (!tokenData) {
+      throw new BadRequestException('Invalid refresh token supplied.');
+    }
+
+    const user: User & any = req.user;
+
+    if (user._id.toString() !== tokenData.userId.toString()) {
+      throw new BadRequestException(
+        'Invalid user with refresh token supplied.',
+      );
+    }
+
+    const payload = this.mapUserToPayload(user);
+    const accessToken = this.generateToken(req, payload);
     return { ...accessToken, ...payload };
   }
 
@@ -78,9 +102,18 @@ export class AuthService {
     return await this.sendEmailVerificationToken(req, user._id);
   }
 
-  private generateToken(payload) {
+  private generateToken(req, payload) {
+    const tokenType = isMobile(req) ? ReqType.MOBILE : ReqType.WEB;
+    const refreshToken = faker.string.uuid();
+    const refreshKey = `Refresh_token:${tokenType}:${refreshToken}`;
+    redisSet(
+      refreshKey,
+      { status: true, userId: payload._id },
+      { EX: 24 * 3600 * 10 },
+    ); // 10 days NOTE: This should alway be longer the the token expire time. Code structure will be improved later across board
     return {
       access_token: global.jwtService.sign(payload),
+      refresh_token: refreshToken,
     };
   }
 }
